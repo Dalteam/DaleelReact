c558eb80e4d7755e664a121fc06046d69e88580c
DaleelTeam <123378061+Dalteam@users.noreply.github.com>
2023-05-23 20:24:44 +0300

    Update user-controller.js

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -55,3 +55,17 @@ export const deleteUser = async (request, response) => {
         response.status(409).json({ message: error.message});     
     }
 }
+export const searchUser  = async (request, respose) => {
+    try{
+        if (!request.query.query){
+            return response.status(200).json([]);
+        }
+        const users = await User.find({staffname: request.query.query}).exec();
+        const roomnumber= await User.find({roomnumber: request.query.query}).exec();
+
+        respose.status(200).json([...users, ...roomnumber]);
+    
+    } catch(error){
+        respose.status(404).json({messeage: error.message })
+    }
+}
